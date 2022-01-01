88ac51ba66fa972444ab86c015a9fdb9223f5435
ravalniravp <ravalniravp12@gmail.com>
2022-01-02 04:39:59 +0530

    End of section 2

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Activity> Activities{ get; set; }
+
+    }
+}
